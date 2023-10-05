2d36f7537856cb1b26da3c8be79f6eba0a2c12ab
wyattb <wbrambl1@students.kennesaw.edu>
2023-10-05 17:51:51 -0400

    .

--- src/DNALib.java
+++ src/DNALib.java
@@ -1,19 +1,24 @@
 public class DNALib {
     public static Boolean validator(String s) {
-        if (s.isEmpty() || s.length() % 3 != 0 || !s.contains("A") ||
-                !s.contains("C") ||
-                !s.contains("G") ||
-                !s.contains("T")) {
+        if ((s.length() % 3 != 0 || s.isEmpty())) {
+            System.out.println("Basics were false");
             return false;
+        } else if (s.contains("A") || s.contains("C") || s.contains("G") || s.contains("T")) {
+            for (int i = 0; i < 3; i++) {
+                if (s.charAt(i) != 'A' && s.charAt(i) != 'C' && s.charAt(i) != 'G' && s.charAt(i) != 'T') {
+                    System.out.println("Wrong letter");
+                    return false;
+                }
+            }
+            if (s.length() > 3) {
+                return validator(s.substring(3));
+            }
         }
-        else{
-            return validator(s);
+            return true;
 
-        }
     }
-
+}
 
 
 
     
-}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        DNALib dna = new DNALib();
+
+        System.out.println(dna.validator("GCCYAT"));
 
 
     }
